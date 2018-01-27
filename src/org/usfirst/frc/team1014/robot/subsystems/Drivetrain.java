d9174bebd5a20b6d9bdbf323df73d4069b479b4c
James Johnson <choiboyjames@gmail.com>
2018-01-27 10:52:26 -0500

    broken code

--- src/org/usfirst/frc/team1014/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team1014/robot/subsystems/Drivetrain.java
@@ -1,17 +1,23 @@
+
 package org.usfirst.frc.team1014.robot.subsystems;
 
 import org.usfirst.frc.team1014.robot.RobotMap;
+import edu.wpi.first.wpilibj.Ultrasonic;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Drivetrain extends Subsystem {
+public class Drivetrain extends Subsystem 
+{
 
 	TalonSRX rightFront, rightBack, leftFront, leftBack;
+	Ultrasonic ultra = new Ultrasonic(0,1);
+	
 
-	public Drivetrain() {
+	public Drivetrain() 
+	{
 		rightFront = new TalonSRX(RobotMap.DRIVE_RIGHT_1_ID);
 		rightBack = new TalonSRX(RobotMap.DRIVE_RIGHT_2_ID);
 		leftFront = new TalonSRX(RobotMap.DRIVE_LEFT_1_ID);
@@ -19,14 +25,19 @@ public class Drivetrain extends Subsystem {
 
 		rightBack.follow(rightFront);
 		leftBack.follow(leftFront);
+		
+		ultra.setAutomaticMode(true);
 	}
 
-	public void directDrive(double left, double right) {
-		rightFront.set(ControlMode.PercentOutput, -right);
-		leftFront.set(ControlMode.PercentOutput, left);
+	public void directDrive(double left, double right) 
+	{
+		System.out.println(ultra.getRangeInches());
+		//rightFront.set(ControlMode.PercentOutput, -right);
+		//leftFront.set(ControlMode.PercentOutput, left);
 	}
 
-	public void initDefaultCommand() {
+	public void initDefaultCommand() 
+	{
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
 	}
