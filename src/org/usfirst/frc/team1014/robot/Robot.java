82d8a893f6595ae1f397315582b8e01f4d439052
Dominik Winecki <dominikwinecki@gmail.com>
2018-01-31 06:36:11 -0500

    Add match time log

--- src/org/usfirst/frc/team1014/robot/Robot.java
+++ src/org/usfirst/frc/team1014/robot/Robot.java
@@ -44,6 +44,7 @@ public class Robot extends TimedRobot {
 
 			BadLog.createTopicStr("System/Browned Out", "bool", () -> LogUtil.fromBool(RobotController.isBrownedOut()));
 			BadLog.createTopic("System/Battery Voltage", "V", () -> RobotController.getBatteryVoltage());
+			BadLog.createTopic("Match Time", "s", () -> DriverStation.getInstance().getMatchTime());
 
 			oi = new OI();
 			driveTrain = new Drivetrain();
