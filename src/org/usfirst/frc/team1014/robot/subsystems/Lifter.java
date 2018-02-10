87460532e5eba1cbf52abb4f7146f44a21a3e64f
ashwinchar <charashwin@gmail.com>
2018-02-10 13:11:06 -0500

    (actually) Added lifter subsystem

---
+++ src/org/usfirst/frc/team1014/robot/subsystems/Lifter.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team1014.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Lifter extends Subsystem {
+
+	
+	
+	@Override
+	protected void initDefaultCommand() {
+	}
+
+}
