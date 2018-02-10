80c442b0a26a61db74d330f6bd0897155ef3d4d9
ashwinchar <charashwin@gmail.com>
2018-02-10 14:39:28 -0500

    Added import for Talon Library

--- src/org/usfirst/frc/team1014/robot/subsystems/Lifter.java
+++ src/org/usfirst/frc/team1014/robot/subsystems/Lifter.java
@@ -2,6 +2,9 @@ package org.usfirst.frc.team1014.robot.subsystems;
 
 import org.usfirst.frc.team1014.robot.RobotMap;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Lifter extends Subsystem {
