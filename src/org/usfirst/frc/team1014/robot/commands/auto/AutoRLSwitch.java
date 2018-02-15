12631a91fbe879b05ae89b3a60153e922437fa99
Tze Hei Tam <18tam.tzehei@gmail.com>
2018-02-14 22:03:24 -0500

    Added a command to release the cube in autonomous

--- src/org/usfirst/frc/team1014/robot/commands/auto/AutoRLSwitch.java
+++ src/org/usfirst/frc/team1014/robot/commands/auto/AutoRLSwitch.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1014.robot.commands.auto;
 
 import org.usfirst.frc.team1014.robot.commands.Spin;
 import org.usfirst.frc.team1014.robot.subsystems.Drivetrain;
+import org.usfirst.frc.team1014.robot.subsystems.Grabber;
 import org.usfirst.frc.team1014.robot.subsystems.Lifter;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -13,9 +14,11 @@ public class AutoRLSwitch extends CommandGroup{
 	 * @param driveTrain
 	 * @param direction - -1 for right, 1 for left
 	 */
-	public AutoRLSwitch(Drivetrain driveTrain, Lifter lifter,int direction) {
+	public AutoRLSwitch(Drivetrain driveTrain, Lifter lifter, Grabber grabber, int direction) {
 		this.addSequential(new AutoRLtoAB(driveTrain));
 		this.addSequential(new Spin(driveTrain, direction * 90));
 		this.addSequential(new AutoRaiseSwitch(lifter));
+		this.addSequential(new AutoRelease(grabber));
+
 	}
 }
--- src/org/usfirst/frc/team1014/robot/commands/auto/AutoSwitchShortFromCenter.java
+++ src/org/usfirst/frc/team1014/robot/commands/auto/AutoSwitchShortFromCenter.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1014.robot.commands.auto;
 
 import org.usfirst.frc.team1014.robot.commands.Spin;
 import org.usfirst.frc.team1014.robot.subsystems.Drivetrain;
+import org.usfirst.frc.team1014.robot.subsystems.Grabber;
 import org.usfirst.frc.team1014.robot.subsystems.Lifter;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -13,9 +14,11 @@ public class AutoSwitchShortFromCenter extends CommandGroup{
 	 * @param driveTrain
 	 * @param direction - -1 for right, 1 for left
 	 */
-	public AutoSwitchShortFromCenter(Drivetrain driveTrain, Lifter lifter,int direction) {
+	public AutoSwitchShortFromCenter(Drivetrain driveTrain, Lifter lifter, Grabber grabber, int direction) {
 		this.addSequential(new AutoCentertoAB(driveTrain, direction));
 		this.addSequential(new Spin(driveTrain, 90 * direction));
 		this.addSequential(new AutoRaiseSwitch(lifter));
+		this.addSequential(new AutoRelease(grabber));
+
 	}
 }
