c3f29d9fb6d899c311081fe165e9bf5a5595dc90
Dominik Winecki <dominikwinecki@gmail.com>
2018-02-19 22:08:25 -0500

    Add limit switches

--- src/org/usfirst/frc/team1014/robot/subsystems/Lifter.java
+++ src/org/usfirst/frc/team1014/robot/subsystems/Lifter.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1014.robot.subsystems;
 
 import org.usfirst.frc.team1014.robot.RobotMap;
+import org.usfirst.frc.team1014.robot.util.LogUtil;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
@@ -12,9 +13,12 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Lifter extends Subsystem {
 
 	TalonSRX liftMotor;
+	DigitalInput bottomLimitSwitch, topLimitSwitch;
 
 	public Lifter() {
 		liftMotor = new TalonSRX(RobotMap.LIFT_1_ID);
+		bottomLimitSwitch = new DigitalInput(0);
+		topLimitSwitch = new DigitalInput(1);
 
 		BadLog.createTopic("Lift/Lifter Output Percent", BadLog.UNITLESS, () -> liftMotor.getMotorOutputPercent(),
 				"hide", "join:Lift/Output Percents");
@@ -24,6 +28,17 @@ public class Lifter extends Subsystem {
 
 		BadLog.createTopic("Lift/Lifter Voltage", "V", () -> liftMotor.getMotorOutputVoltage(), "hide",
 				"join:Lift/Output Voltages");
+
+		BadLog.createTopicStr("Lift/Bottom Limit Switch", "bool", () -> LogUtil.fromBool(isAtBottom()));
+		BadLog.createTopicStr("Lift/Top Limit Switch", "bool", () -> LogUtil.fromBool(isAtTop()));
+	}
+
+	private boolean isAtTop() {
+		return topLimitSwitch.get();
+	}
+
+	private boolean isAtBottom() {
+		return !bottomLimitSwitch.get();
 	}
 
 	public void move(double speed) {
@@ -32,8 +47,14 @@ public class Lifter extends Subsystem {
 	}
 
 	public void safeMove(double speed) {
+		if(speed < 0) {
+			if(isAtBottom())
+				speed = 0;
+		} else {
+			if(isAtTop())
+				speed = 0;
+		}
 		liftMotor.set(ControlMode.PercentOutput, speed);
-
 	}
 
 	@Override
